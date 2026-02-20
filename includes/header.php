d35db2935015993044c461885174a6a744c88d94
ShivsterNk <siva.nirmalakumar@gmail.com>
2026-02-19 19:52:50 -0500

    Updated project files: modified the CSS, PHP, deleted old JS/html

---
+++ includes/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Smart Plate</title>
+        <link rel="stylesheet" href="assets/spstyle.css">
+    </head>
+    <body>
+        <header>
+            
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="plan.php">Plan</a>
+                <a href="signin.php">Sign In</a>
+                <a href="premeals.php">Pre-Meals</a>
+            </nav>
+        </header>
