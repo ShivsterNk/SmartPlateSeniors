22c9d1656e5e78784e1526557cb217864068436e
Derek Mendez <derekmenvale@gmail.com>
2026-03-01 18:01:13 -0500

    Edited comments

--- config/api-keys.example.php
+++ config/api-keys.example.php
@@ -2,7 +2,7 @@
 
 /**
  * API Keys Configuration - EXAMPLE FILE
- * 1. Create api-keys.php and add your real keys
+ * 1. Create api-keys.php and then paste the real key
  */
 
 define('FDC_API_KEY', 'PASTE_API_KEY_HERE');
