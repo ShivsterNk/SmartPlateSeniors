eb4248029a4738f17e123ef6428b235b10c4e6ee
kemi-lee <eunicedigitals64@gmail.com>
2026-03-04 10:38:08 -0500

    API Key file's instructions are more simpler

--- Nutrition Explore Page/nutrition-explorer.php
+++ Nutrition Explore Page/nutrition-explorer.php
@@ -42,7 +42,7 @@
   </main>
 
   <script>
-      const API_KEY = '<?php require_once __DIR__ . "/../config/api-keys.php"; echo FDC_API_KEY; ?>';
+      const API_KEY = '<?php require_once __DIR__ . "/../apiKey/api-keys.php"; echo FDC_API_KEY; ?>';
   </script>
   <script src="nutrition-explorer.js"></script>
 </body>
--- config/api-keys.example.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * API Keys Configuration - EXAMPLE FILE
- * 1. Create api-keys.php and then paste the real key
- */
-
-define('FDC_API_KEY', 'PASTE_API_KEY_HERE');
