93b53c45a73681ee810d99207957d6d7d5776770
kelle <kellje1@farmingdale.edu>
2026-03-20 20:28:44 -0400

    Save current work before syncing

--- meal.js
+++ meal.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 const leftButton = document.querySelector(".leftButton");
 const rightButton = document.querySelector(".rightButton");
 const slider = document.querySelector("#slider");
@@ -22,8 +23,20 @@ const slider = document.querySelector("#slider");
 rightButton.addEventListener("click", () => {
     slider.scrollLeft += 300; 
 });
+=======
+const leftButton = document.querySelector(".leftButton");
+const rightButton = document.querySelector(".rightButton");
+const slider = document.querySelector("#slider");
+>>>>>>> cc713e9 (Save current work before syncing)
 
 leftButton.addEventListener("click", () => {
-    slider.scrollLeft -= 300; 
+    slider.scrollLeft -= 300;
 });
+<<<<<<< HEAD
 >>>>>>> 4d0e039 (Final)
+=======
+
+rightButton.addEventListener("click", () => {
+    slider.scrollLeft += 300;
+});
+>>>>>>> cc713e9 (Save current work before syncing)
