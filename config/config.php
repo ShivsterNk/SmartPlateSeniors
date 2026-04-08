803b5db626bd0136710b80701c0444890a35562e
ShivsterNk <siva.nirmalakumar@gmail.com>
2026-04-08 13:03:44 -0400

    Fixed up the config.php file to allow for all Local Machines to Work without crashing on other people's computers.

--- config/config.php
+++ config/config.php
@@ -2,25 +2,33 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'smart_plate_db');
 define('DB_USER', 'root');
-/*define('DB_PASS', 'root');*/
-define('DB_PORT', '8889');
 
-if (file_exists('C:\Program Files\Ampps')) {
-    // AMPPS on Windows
-    // define('DB_PASS', 'mysql');
-    $db_pass = 'mysql';
-} elseif (file_exists('/Applications/AMPPS')) {
-    // AMPPS on macOS
-    // define('DB_PASS', 'mysql');
-    $db_pass = 'mysql';
-} elseif (file_exists('/Applications/MAMP')) {
-    // MAMP on macOS
-    //define('DB_PASS', 'root');
-    $db_pass = 'root';
-} else {
-    // Default (e.g. XAMPP)
-    //define('DB_PASS', '');
+if (file_exists('C:\xampp')) {
+    // XAMPP on Windows
     $db_pass = '';
+    $db_port = '3306';
+} 
+elseif (file_exists('C:\Program Files\Ampps')) {
+    // AMPPS on Windows
+    $db_pass = 'mysql';
+    $db_port = '3306';
+} 
+elseif (file_exists('/Applications/AMPPS')) {
+    // AMPPS on macOS
+    $db_pass = 'mysql';
+    $db_port = '3306';
+} 
+elseif (file_exists('/Applications/MAMP')) {
+    // MAMP on macOS
+    $db_pass = 'root';
+    $db_port = '8889';
+} 
+else {
+    // Default (e.g. XAMPP)
+    $db_pass = '';
+    $db_port = '3306';
 }
 
+//Define Constants
 define('DB_PASS', $db_pass);
+define('DB_PORT', $db_port);
